af75ea060d43683edd66f374121114ffd39fb1d8
lat9 <cindy@vinosdefrutastropicales.com>
2026-06-14 13:48:06 -0400

    Update plugin version/manifest to v3.0.1

--- zc_plugins/DownloadAlreadyPurchased/v3.0.1/manifest.php
+++ zc_plugins/DownloadAlreadyPurchased/v3.0.1/manifest.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'pluginVersion' => 'v3.0.0',
+    'pluginVersion' => 'v3.0.1',
     'pluginName' => 'Download Already Purchased',
     'pluginDescription' => 'If a customer has previously purchased a download and that product is still available for the customer\'s download, let the customer know.',
     'pluginAuthor' => 'lat9',
